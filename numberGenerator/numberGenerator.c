db53bd7519ee480011214137f2770d10666eb294
Victor2752 <victor.oliveira@sqginfo.com.br>
2023-09-13 09:31:16 -0300

    corrigido gerador de numeros

--- numberGenerator/numberGenerator.c
+++ numberGenerator/numberGenerator.c
@@ -3,25 +3,60 @@
 #include <string.h>
 #include <time.h>
 
-char numeroOriginal[6] = "820187";
+// char numeroOriginal[6] = "820187";
+char numeroOriginal[6] = "770172";
+// Definindo a data alvo (09/03/2023)
+struct tm target_date = {
+    tm_year : 2023 - 1900,
+    tm_mon : 2,
+    tm_mday : 9
+};
 char *ptrNumeroOriginal = numeroOriginal;
-int n0,n1,n2,n3,n4,n5;
 
-int soma(int num) {
-    if (num == 9) {
+int soma(int num)
+{
+    if (num == 9)
+    {
         return 0;
-    } else {
+    }
+    else
+    {
         return num + 1;
     }
 }
 
-int calculaDias() {
-    // Definindo a data alvo (14/08/2023)
-    struct tm target_date = {0};
-    target_date.tm_year = 2023 - 1900; // Ano - 1900
-    target_date.tm_mon = 7; // Mês (0-11, então 7 representa agosto)
-    target_date.tm_mday = 14; // Dia do mês
+int soma2(int num)
+{
+    if (num == 9)
+    {
+        return 1;
+    }
+    else
+    {
+        return num + 2;
+    }
+}
 
+int *map2int()
+{
+    int *num = malloc(6 * sizeof(int));
+    for (int j = 0; j < 6; j++)
+    {
+        num[j] = ptrNumeroOriginal[j] - '0';
+    }
+    return num;
+}
+
+void map2char(int *num)
+{
+    for (int j = 0; j < 6; j++)
+    {
+        ptrNumeroOriginal[j] = num[j] + '0';
+    }
+}
+
+int calculaDias()
+{
     // Obtendo a data atual do sistema
     time_t current_time;
     time(&current_time);
@@ -33,33 +68,80 @@ int calculaDias() {
     // Convertendo a diferença de segundos para dias
     int diff_days = (int)(diff_seconds / (60 * 60 * 24));
 
-    printf("Diferença em dias entre 14/08/2023 e a data atual: %d dias\n", diff_days);
-    return diff_days;
+    // Calculando a diferença em anos
+    int diff_years = current_date->tm_year - target_date.tm_year;
+
+    // Calculando a diferença em meses
+    int diff_months = diff_years * 12 + (current_date->tm_mon - target_date.tm_mon);
+
+    // Calculando o dia do mês
+    int day_month = current_date->tm_mday;
+
+    if (diff_months > 0)
+    {
+        int *num = map2int();
+        num[0] = 7;
+        num[1] = 9;
+        for (int i = 0; i < diff_months; i++)
+        {
+            num[3] = soma2(num[3]);
+        }
+        num[4] = 7;
+        num[5] = 4;
+        map2char(num);
+    }
+
+    // printf("Diferença em dias entre 14/08/2023 e a data atual: %d dias\n", diff_days);
+    // printf("Diferença em meses entre 14/08/2023 e a data atual: %d meses\n", diff_months);
+    return day_month;
+}
+
+void calcula_numero(int diff_days)
+{
+    int *num = map2int();
+    printf("%02d/%02d - %s\n", 1, 1, numeroOriginal);
+    for (int i = 2; i <= diff_days; i++)
+    {
+        num[5] = soma(num[5]);
+        num[1] = soma(num[1]);
+        if (num[1] == 8)
+        {
+            num[0] = soma(num[0]);
+            num[4] = soma(num[4]);
+        }
+        map2char(num);
+        printf("%02d/%02d - %s\n", i, i, numeroOriginal);
+    }
 }
 
 int main()
 {
-    int diff_days = calculaDias();
-    int num[6];
-    for (int i = 1; i <= diff_days; i++)
+    // Obtendo a data atual do sistema
+    time_t current_time;
+    time(&current_time);
+    struct tm *current_date = localtime(&current_time);
+
+    // Calculando a diferença em anos
+    int diff_years = current_date->tm_year - target_date.tm_year;
+    // Calculando a diferença em meses
+    int diff_months = diff_years * 12 + (current_date->tm_mon - target_date.tm_mon);
+
+    if (diff_months > 0)
     {
-        for (int j = 0; j < 6; j++)
+        int *num = map2int();
+        num[0] = 7;
+        num[1] = 9;
+        for (int i = 0; i < diff_months; i++)
         {
-            num[j] = ptrNumeroOriginal[j] - '0';
+            num[3] = soma2(num[3]);
         }
-        num[5] = soma(num[5]);
-        num[1] = soma(num[1]);
-        if (num[1] == 8) {
-            num[0] = soma(num[0]);
-            num[4] = soma(num[4]);
-        }
-        for (int j = 0; j < 6; j++)
-        {
-            ptrNumeroOriginal[j] = num[j] + '0';
-        }
-        printf("dia  %02d, numero: %d%d%d%d%d%d\n",i,num[0],num[1],num[2],num[3],num[4],num[5]);
+        num[4] = 7;
+        num[5] = 4;
+        map2char(num);
     }
 
-    printf("o numero final para o dia %d é: %s\n\n", diff_days, numeroOriginal);
+    calcula_numero(current_date->tm_mday);
+
+    printf("o numero final para o dia %d é: %s\n\n", current_date->tm_mday, numeroOriginal);
     return 0;
 }
